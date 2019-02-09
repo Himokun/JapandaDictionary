b0e20843fbd216a2a06ee7ca8403964f4bdbf331
Himokun <41708309+Himokun@users.noreply.github.com>
2019-02-09 09:48:23 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,38 +1,35 @@
 //const FADE_TIME = 600;
 
+const FADE_TIME = 600;
+
 function startApp() {
   watchSubmit();
  }
 
+
+
 function watchSubmit() {
   $('.startForm').submit(event => {
     event.preventDefault();
+	$('.loading-animation').show();
     $('.results').hide();
     $('.js-results').html('');
-    let searchTerm = document.getElementsByClassName('.search-text')
-    let searchTermLowercase = searchTerm.toLowerCase().trim();
+    $('.word-title').toggle();
+    $('.definition-title').toggle();
+    let searchTerm = document.getElementById('search-text-id');
+    let searchTermLowercase = searchTerm.value.toLowerCase().trim();
 
-    $(event.currentTarget)
-      .find('.js-query')
-      .select();
+    getWordFromApi(searchTermLowercase, processDataFromWordApi, 'ja');
+	  
+     $(event.currentTarget)
+        .find('.js-query')
+	   	.select();
   });
-}
 
-function getWordFromApi(searchTerm, callback) {
-  const query = {
-    url:
-      'http://beta.jisho.org/api/v1/search/words',
-    success: callback,
-    data: {
-      keyword: searchTerm
-    }
-  };
-  $.ajax(query);
-}
-
-function processDataFromWordApi(data) {
-  $('.results-count').text(`(${data.data.length})`);
-  if (data.data.length === 0) {
+ function processDataFromWordApi(data) {
+  const {data : {translations}} = data 
+  $('.results-count').text(`(${translations.length})`);
+  if (translations.length === 0) {
     $('.js-results').html(`form class="startForm'>
         <fieldset name="search-bar'>
           Sorry! Your search didn't return any results.
@@ -40,49 +37,50 @@ function processDataFromWordApi(data) {
       </div>`);
     $('.results').show();
   } else {
-    processWordData(data.data);
+    displayWordData(translations);
   }
 }
 
-function processWordData(wordArray) {
-  let currentWordArray = wordArray.slice();
-  let wordsToDisplay = [];
-  if (currentWordArray.length <= 5) {
-    wordsToDisplay = currentWordArray.slice();
-} else {
-    wordsToDisplay = currentWordArray.splice(0, 5);
-  }
+  $('.bar').submit(event => {
+    event.preventDefault();
+	$('.loading-animation').show();
+    $('.results').hide();
+    $('.js-results').html('');
+    let searchTerm = document.getElementById('js-search-term');
+    let searchTermLowercase = searchTerm.value.toLowerCase().trim();
 
- displayWordData(
-      wordsToDisplay,
-      scrollListenerActive,
-      wordsRemainingToDisplay
-    );
- }
-function displayWordData(
-  wordArray,
-  scrollListenerActive,
-  wordsRemainingToDisplay
-) {
+    getWordFromApi(searchTermLowercase, processDataFromWordApi, 'en');
+	  
+     $(event.currentTarget)
+        .find('.js-query')
+	   	.select();
+  });
+}
+
+function getWordFromApi(searchTerm, callback, lang) {
+  const query = {
+    url:
+      `https://translation.googleapis.com/language/translate/v2?q=${searchTerm}&target=${lang}&key=AIzaSyCN13ROG4x24_PslJSVFvJnKwINesNbyo4`,
+    success: callback,
+    data: {
+    keyword: searchTerm
+    }
+  };
+  $.ajax(query);
+}
+
+function processWordData(wordArray) {
+   displayWordData(wordsToDisplay);
+}
+ 
+function displayWordData( wordArray) {
   $('.loading-animation').hide();
   $('.results').fadeIn(FADE_TIME);
-    let wordResultSection = '';
-    if (typeof japaneseWord === 'undefined') {
-      wordResultSection = `
-        <span class="japanese-word-hiragana">${wordReading}</span> <span class='word-label'>(word)</span><br>
-        <span class="japanese-word-romaji">${wordRomaji}</span> <span class='word-label'>(romaji)</span><br>`;
-    } else {
-      wordResultSection = `
-        <span class="japanese-word">${japaneseWord}</span> <span class='word-label'>(word)</span><br>
-        <span class="japanese-word-hiragana">${wordReading}</span> <span class='word-label'>(reading)</span><br>
-        <span class="japanese-word-romaji">${wordRomaji}</span> <span class='word-label'>(romaji)</span><br>`;
-    }
-    let definitionSection = wordDefinitions
-      .map((definition, index) => {
-        return `<div class="definition"><span class='definition-index'>
-          ${index + 1})</span> ${definition}</span></div>`;
-      })
-      .join('');
+	
+	const wordReading = wordArray[0].translatedText;
+		let wordResultSection = `
+        <span class="japanese-word-hiragana">${wordReading}</span> <span class='word-label'>(word)</span><br>`;
+
     let $resultDiv = $(`
       <div class="row">
         <div class="col-3">
@@ -90,18 +88,12 @@ function displayWordData(
             ${wordResultSection}
           </div>
         </div>
-        <div class="col-5">
-          <div class="definition-result">
-            ${definitionSection}
-          </div>
-        </div>
+      </div>  
        `);
     $resultDiv
       .hide()
       .appendTo('.js-results')
       .fadeIn(FADE_TIME);
-
-  if (scrollListenerActive) pageScrollListener(wordsRemainingToDisplay);
 }
 
 function pageScrollListener(wordArray) {
@@ -114,4 +106,5 @@ function pageScrollListener(wordArray) {
   });
 }
 
+$(startApp);
 $(startApp);
